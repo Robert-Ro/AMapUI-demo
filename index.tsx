05ec3fbd48aa48016d163270823fc6b1c4269060
Liutsing Robert <52750222+Robert-Ro@users.noreply.github.com>
2022-11-02 17:53:34 +0800

    Initial commit

---
+++ index.tsx
@@ -0,0 +1,15 @@
+/**
+* This is an auto-generated demo by dumi
+* if you think it is not working as expected,
+* please report the issue at
+* https://github.com/umijs/dumi/issues
+**/
+import React from 'react';
+import { createRoot } from "react-dom/client";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
+
+root.render(<App />);
